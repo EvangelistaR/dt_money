b6da73e79dee06e99408877ced7df3971cf961a5
Rafael Evangelista <rafael.evans@live.com>
2022-07-05 22:32:57 -0300

    list transactions from api

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,29 @@ createServer({
   models: {
     transaction: Model,
   },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freela website',
+          type: 'deposit',
+          category: 'Development',
+          amount: 6000,
+          createdAt: new Date('2022-02-01 09:10'),
+        },
+        {
+          id: 2,
+          title: 'Internet',
+          type: 'withdraw',
+          category: 'Custo',
+          amount: 150,
+          createdAt: new Date('2022-02-01 09:10'),
+        }
+      ],
+    })
+  },
   routes() {
     this.namespace = 'api';
 
